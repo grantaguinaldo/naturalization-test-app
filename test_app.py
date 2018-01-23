79c177079bbaba9d417f4db78aaba069c7828620
GTA <gta@GTAs-MacBook-Pro.local>
2018-01-23 00:09:42 -0800

    Update

--- test_app.py
+++ test_app.py
@@ -2,6 +2,6 @@ def test_app():
 	import random
 	from test_questions import test_questions
     num = random.randint(0, 101)
-        question = test_questions[num]
+    question = test_questions[num]
 
     return question
